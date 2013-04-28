85be4172b09c01ec3723d77f8d83e7dbf5480198
David Harris <lolidunno@live.co.uk>
2013-04-27 17:10:23 -0700

    Update usage.php

--- usage.php
+++ usage.php
@@ -22,18 +22,14 @@
 			$filename = __DIR__ . '/tmp/' . md5($_POST['php'] . time()) . '.tmp.php';
 			file_put_contents($filename, $_POST['php']);
 			chmod($filename, 0777);
-			$iterator = new Orpheus\PHPSmallify($filename);
-
-			$encodeStrings = true;
-			$stripWhiteSpace = true;
-			$results = $iterator->smallify();
+			$phpSmallify = new Orpheus\PHPSmallify($filename);
+			$results = $phpSmallify->smallify();
 
 			echo '<textarea name="php">' . htmlentities($results['smallified']) . '</textarea>';
 		}
 		echo '
 		<form method="POST" action="">
 			<textarea name="php"></textarea>
-			<input type="checkbox" name="stripComments" value="1">Remove comments <input type="checkbox" name="encodeStrings" value="1">Encode strings <input type="checkbox" name="stripWhitespace" value="1">Remove whitespace
 			<p><input type="submit" value="Minify" /></p>
 		</form>';
 		?>
